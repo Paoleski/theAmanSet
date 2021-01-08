7f42c61bb17756b951209eefc5f9bd67e78b5a43
Paoleski <lucas.paoleschi@gmail.com>
2021-01-08 19:53:08 -0300

    responsiveness and a few design changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { AnimatePresence } from 'framer-motion';
 import Home from './components/Home';
 import './styles/App.css';
-import { Breakpoint, BreakpointProvider } from 'react-socks';
+
 
 function App() {
   return (
     <div className="App">
-      <BreakpointProvider>
+     
         <AnimatePresence>
           <Home />
         </AnimatePresence>
-      </BreakpointProvider>
+     
     </div>
   );
 }
